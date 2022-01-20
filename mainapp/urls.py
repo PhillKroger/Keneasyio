1434030726b1503e7af9673000852a6d0a3fb2c2
MaksimBondarev <max.bondarev2005@gmail.com>
2022-01-20 17:31:05 +0300

    #

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.mainapp, name="mainapp"),
     path('filter/', views.ProductListView.as_view(), name='index'),
-    # path('results/', views.results_view, name='results'),
+    path('about/', views.about, name='about'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -6,7 +6,11 @@ from .models import Product
 
 
 def mainapp(request):
-    return render(request, "mainapp/main.html")
+    return render(request, "mainapp/index.html")
+
+
+def about(request):
+    return render(request, 'mainapp/about.html')
 
 
 class ProductListView(ListView):
@@ -26,7 +30,7 @@ class ProductListView(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['filter'] = ProductFilter(self.request.GET, queryset=self.get_queryset())
-        print(context)
+
         return context
 
 
