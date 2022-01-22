fa2d232968a5f270ed3693ee235dc32e2af770cd
MaksimBondarev <max.bondarev2005@gmail.com>
2022-01-22 21:02:24 +0300

    #

--- mainapp/filters.py
+++ mainapp/filters.py
@@ -5,5 +5,8 @@ from .models import Product
 class ProductFilter(filters.FilterSet):
     class Meta:
         model = Product
-        fields = ('category_season', 'category_size', 'category_price')
+        fields = ('category_season',
+                  'category_size',
+                  'category_price'
+                  )
 
