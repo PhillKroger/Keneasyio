40ae3d2581093c8ceff2b81748eeb92e9488e834
MaksimBondarev <max.bondarev2005@gmail.com>
2022-01-22 19:58:12 +0300

    #

--- mainapp/views.py
+++ mainapp/views.py
@@ -30,7 +30,6 @@ class ProductListView(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['filter'] = ProductFilter(self.request.GET, queryset=self.get_queryset())
-
         return context
 
 
