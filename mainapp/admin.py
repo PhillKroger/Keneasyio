668c6e0c7dab1e9b6390ceb282b4ecb3969ff848
MaksimBondarev <max.bondarev2005@gmail.com>
2022-01-23 13:38:56 +0300

    #

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import CategorySeason, CategoryClothes, CategorySize, CategoryPrice, Product
+from .models import CategorySeason, CategoryClothes, CategorySize, CategoryPrice, CategoryImage, Product
 
 admin.site.register(CategorySeason)
 admin.site.register(CategoryClothes)
 admin.site.register(CategorySize)
 admin.site.register(CategoryPrice)
+admin.site.register(CategoryImage)
 admin.site.register(Product)
 
--- mainapp/models.py
+++ mainapp/models.py
@@ -13,11 +13,12 @@ class CategorySeason(models.Model):
 
 class CategoryClothes(models.Model):
     name = models.CharField(max_length=255, verbose_name='category_clothes_name')
-    need_in_set = models.IntegerField(verbose_name='weight_in_set', null=True, default=0)
+    # need_in_set = models.IntegerField(verbose_name='weight_in_set', null=True, default=0)
+    img = models.ImageField(verbose_name='img', upload_to='media/categories', null=True)
     slug = models.SlugField(unique=True, null=True)
 
     def __str__(self):
-        return "{} {}".format(self.name, self.need_in_set)
+        return "{}".format(self.name)
 
 
 class CategorySize(models.Model):
@@ -37,10 +38,22 @@ class CategoryPrice(models.Model):
         return self.name
 
 
+class CategoryImage(models.Model):
+    name = models.CharField(max_length=255, verbose_name='category_image_name')
+    img = models.ImageField(verbose_name='img', upload_to='media/category_image', null=True)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
 class Product(models.Model):
 
     category_season = models.ForeignKey(
         CategorySeason, verbose_name='category_season', on_delete=models.CASCADE, blank=True, null=True
+    )
+
+    category_image = models.ForeignKey(
+        CategoryImage, verbose_name='category_Image', on_delete=models.CASCADE, blank=True, null=True
     )
 
     category_clothes = models.ForeignKey(
@@ -57,6 +70,7 @@ class Product(models.Model):
     color = models.CharField(max_length=255, verbose_name='color', null=True)
     product_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     slug = models.SlugField(unique=True, null=True)
+
 
     def __str__(self):
         return '{} {} {} {} {}'.format(self.slug, self.category_season, self.category_clothes, self.category_size, self.category_price)
