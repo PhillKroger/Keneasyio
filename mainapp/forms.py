5755008fccaa2f9e7949282d829741c5a6592c0b
MaksimBondarev <66637696+PhillKroger@users.noreply.github.com>
2022-04-14 17:32:13 +0300

    Update forms.py

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -17,3 +17,20 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'email', 'text',)
 
+
+        
+# new
+class ProductForm(forms.ModelForm):
+    category_season = forms.ModelChoiceField(queryset=CategorySeason.objects.all())
+    category_clothes = forms.ModelChoiceField(queryset=CategoryClothes.objects.all())
+    category_size = forms.ModelChoiceField(queryset=CategorySize.objects.all())
+    category_price = forms.ModelChoiceField(queryset=CategoryPrice.objects.all())
+    text = forms.CharField(
+        label='Прикрепите фотографию докумета(удостоверения) о владении магазином одежды. Фото нужно перетащить в '
+              'поле ввода снизу',
+        widget=CKEditorWidget(attrs={'class': 'post_form_text', 'id': 'post_form_text_label'}))
+
+    class Meta:
+        model = Product
+        fields = ('category_size', 'category_price', 'category_season', 'category_clothes', 'img',)
+
