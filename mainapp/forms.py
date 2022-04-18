377aa45940b3ea895272fa235757c083fd601b80
MaksimBondarev <max.bondarev2005@gmail.com>
2022-04-18 14:24:03 +0300

    #

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -17,3 +17,32 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'email', 'text',)
 
+
+class ProductForm(forms.ModelForm):
+    category_season = forms.ModelChoiceField(queryset=CategorySeason.objects.all())
+    category_clothes = forms.ModelChoiceField(queryset=CategoryClothes.objects.all())
+    category_size = forms.ModelChoiceField(queryset=CategorySize.objects.all())
+    category_price = forms.ModelChoiceField(queryset=CategoryPrice.objects.all())
+    text = forms.CharField(
+        label='Прикрепите фотографию докумета(удостоверения) о владении магазином одежды. Фото нужно перетащить в '
+              'поле ввода снизу',
+        widget=CKEditorWidget(attrs={'class': 'post_form_text', 'id': 'post_form_text_label'}))
+
+    class Meta:
+        model = Product
+        fields = ('category_size', 'category_price', 'category_season', 'category_clothes', 'img',)
+
+
+class SetForm(forms.ModelForm):
+    # Post.objects.filter(author=user)
+    product1 = forms.ModelChoiceField(queryset=Product.objects.all())
+    product2 = forms.ModelChoiceField(queryset=Product.objects.all())
+    product3 = forms.ModelChoiceField(queryset=Product.objects.all())
+    product4 = forms.ModelChoiceField(queryset=Product.objects.all())
+    product5 = forms.ModelChoiceField(queryset=Product.objects.all())
+    #product5 = forms.ModelChoiceField(queryset=Product.objects.all())
+
+    class Meta:
+        model = Set
+        fields = ('product1', 'product2', 'product3', 'product4', 'product5', )
+
